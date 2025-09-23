5078b5a7bbee01021a44970a84abe5c89f56fbb4
8VINO <viniciussouto1008@gmail.com>
2025-09-23 17:59:17 -0300

    feat:desenvolve produto

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -3,6 +3,7 @@ package br.com.ifpe.oxefood.api.produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.produto.Produto;
 import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("api/produto")
 public class ProdutoController {
